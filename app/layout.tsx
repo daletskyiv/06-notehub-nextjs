16346b97ad80fcb3b1161c4525f0e8d460eba75c
Vladyslav Daletskyi <63199353+daletskyiv@users.noreply.github.com>
2026-06-11 15:35:37 +0300

    changing files to next.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header/Header';
 import './globals.css';
 import Footer from '@/components/Footer/Footer';
+import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -9,9 +10,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body>
-        <Header />
-        <main>{children}</main>
-        <Footer />
+        <TanStackProvider>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </TanStackProvider>
       </body>
     </html>
   );
